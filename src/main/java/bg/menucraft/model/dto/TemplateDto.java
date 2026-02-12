c1cc0eac75783f41acfaa8d58b8460220b9832b3
Blagovest Kirilov <blagovestkirilov1@gmail.com>
2026-02-12 20:10:51 +0200

    Getting template by venue name.

--- src/main/java/bg/menucraft/model/dto/TemplateDto.java
+++ src/main/java/bg/menucraft/model/dto/TemplateDto.java
@@ -5,21 +5,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class TemplateDto {
-
-    private UUID id;
     private String name;
     private String description;
     private String data;
     private String contentType;
-    private List<TemplateSectionDto> sections = new ArrayList<>();
+    private List<TemplateSectionDto> sections;
     private Instant createdAt;
     private Instant updatedAt;
 }
